d281fd97491c75f23071e76dcba93bbb5b24d624
kafri8889 <kafri8889@gmail.com>
2023-12-09 21:48:58 +0700

    Fix bug

--- app/src/main/java/com/anafthdev/githubuser/data/datasource/remote/ApiClient.kt
+++ app/src/main/java/com/anafthdev/githubuser/data/datasource/remote/ApiClient.kt
@@ -2,6 +2,7 @@ package com.anafthdev.githubuser.data.datasource.remote
 
 import com.anafthdev.githubuser.BuildConfig
 import com.anafthdev.githubuser.data.Constant
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,17 +10,30 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        setLevel(if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE)
+    private val loggingInterceptor: HttpLoggingInterceptor by lazy {
+        HttpLoggingInterceptor().apply {
+            setLevel(if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE)
+        }
     }
 
-    private val client by lazy {
+    private val authInterceptor: Interceptor by lazy {
+        Interceptor { chain ->
+            chain.proceed(
+                chain.request().newBuilder()
+                    .addHeader("Authorization", Constant.GITHUB_TOKEN)
+                    .build()
+            )
+        }
+    }
+
+    private val client: OkHttpClient by lazy {
         OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
             .addInterceptor(loggingInterceptor)
             .build()
     }
 
-    private val retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(Constant.GITHUB_API_ENDPOINT)
             .client(client)
@@ -27,7 +41,7 @@ object ApiClient {
             .build()
     }
 
-    val githubApiService by lazy {
+    val githubApiService: GithubApiService by lazy {
         retrofit.create(GithubApiService::class.java)
     }
 
--- app/src/main/java/com/anafthdev/githubuser/data/datasource/remote/GithubApiService.kt
+++ app/src/main/java/com/anafthdev/githubuser/data/datasource/remote/GithubApiService.kt
@@ -1,11 +1,9 @@
 package com.anafthdev.githubuser.data.datasource.remote
 
-import com.anafthdev.githubuser.data.Constant
 import com.anafthdev.githubuser.data.model.SearchResponse
 import com.anafthdev.githubuser.data.model.User
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -13,31 +11,25 @@ interface GithubApiService {
 
     @GET("search/users")
     fun search(
-        @Query("q") username: String,
-        @Header("Authorization") token: String = Constant.GITHUB_TOKEN
+        @Query("q") username: String
     ): Call<SearchResponse>
 
     @GET("users")
-    fun getUsers(
-        @Header("Authorization") token: String = Constant.GITHUB_TOKEN
-    ): Call<List<User>>
+    fun getUsers(): Call<List<User>>
 
     @GET("users/{username}")
     fun getDetail(
-        @Path("username") username: String,
-        @Header("Authorization") token: String = Constant.GITHUB_TOKEN
+        @Path("username") username: String
     ): Call<User>
 
     @GET("users/{username}/followers")
     fun getFollowers(
-        @Path("username") username: String,
-        @Header("Authorization") token: String = Constant.GITHUB_TOKEN
+        @Path("username") username: String
     ): Call<List<User>>
 
     @GET("users/{username}/following")
     fun getFollowing(
-        @Path("username") username: String,
-        @Header("Authorization") token: String = Constant.GITHUB_TOKEN
+        @Path("username") username: String
     ): Call<List<User>>
 
 }
